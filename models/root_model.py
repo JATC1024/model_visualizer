0035b83d3c0a74605cddf5d9bf49513730dfd626
Trung-Hieu Ho <piouscity@outlook.com>
2019-06-20 22:31:32 +0700

    root_model arguments

--- models/root_model.py
+++ models/root_model.py
@@ -5,9 +5,21 @@ class root_model:
 
 	def __init__(self):
 		self.name = 'Root Model'
-
+	
+	# User choose a tag
 	def recognize(self, tag):
 		pass
-
-
-
+	
+	# Return true if not converge
+	def next_step(self):
+		pass
+		
+	# Return array of arguments needed for model to train
+	# Each argument is a pair of 'name' and 'type'
+	def get_arguments(self):
+		pass
+	
+	# Set value of arguments needed for model to train
+	# Each element of array is a value
+	def set_arguments(self, array):
+		pass
