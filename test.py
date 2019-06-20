e404e3d38f7bf55d01aede86fa69daf64b9d6427
JATC1024 <JATC1024@gmail.com>
2019-06-20 23:14:26 +0700

    add code to test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from models import model_store
 if __name__ == '__main__':
 	if (model_store.recognize('SVM')):
 		model = model_store.current
+		print(model.get_arguments())
 		print('OK')
 	else:
 		print('Not OK')
