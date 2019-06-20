2a83f4f3c19cdf5cad8f7e722a3044f0f5808e69
JATC1024 <JATC1024@gmail.com>
2019-06-21 00:15:58 +0700

    add config for svm

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
-from models import model_store
+from model_store import model_store
 
 if __name__ == '__main__':
-	if (model_store.recognize('SVM')):
-		model = model_store.current
-		print(model.get_arguments())
-		print('OK')
-	else:
-		print('Not OK')
+	ms = model_store()
+	ms.recognize('SVM')
+	print(ms.current.get_arguments())
