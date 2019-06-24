7bececad799ea3d73613eaa7bfd743760e6b4ecd
JATC1024 <JATC1024@gmail.com>
2019-06-24 21:55:12 +0700

    add test

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-from model_store import model_store
+from models.model_prototype import model_prototype
+from models.computational_graph import Graph
 
 if __name__ == '__main__':
-	ms = model_store()
-	ms.recognize('SVM')
-	print(ms.current.get_arguments())
+	# md = model_prototype.create_model("Support Vector Machines")
+	# md.print()
+	Graph().as_default()
+	x = Placeholder()
+	y = Placeholder()
+	plus = Add(x, y)
+	sess = Session()
+	output = sess.run(plus, {x : [2, 3], y : [3, 1]})
+	print(output)
