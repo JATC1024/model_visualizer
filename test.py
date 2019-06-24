d8e2ed6889066280b10317d12053c957ac3a058b
Trung-Hieu Ho <piouscity@outlook.com>
2019-06-24 23:59:28 +0700

    GUI

--- test.py
+++ test.py
@@ -1,5 +1,90 @@
-import Tkinter
+#!/usr/bin/python
 
-top = Tkinter.Tk()
+import tkinter as tk
+from tkinter import ttk
 
-top.mainloop()
+def on_tab_selected(event):
+	selected_tab = event.widget.select()
+	tab_text = event.widget.tab(selected_tab, "text")
+	if tab_text == "All Records":
+		print("All Records tab selected")
+	if tab_text == "Add New Record":
+		print("Add New Record tab selected")
+
+# Main form
+form = tk.Tk()
+form.title("Tkinter Tabs example")
+form.geometry("500x280")
+
+# Create tab parent
+tab_parent = ttk.Notebook(form)
+
+# Create child tabs
+tab1 = ttk.Frame(tab_parent)
+tab2 = ttk.Frame(tab_parent)
+
+# Assign child tabs to their parents
+tab_parent.add(tab1, text = "All Records")
+tab_parent.add(tab2, text = "Add New Record")
+
+# === WIDGETS FOR TAB ONE
+firstLabelTabOne = tk.Label(tab1, text="First Name:")
+familyLabelTabOne = tk.Label(tab1, text="Family Name:")
+jobLabelTabOne = tk.Label(tab1, text="Job Title:")
+
+firstEntryTabOne = tk.Entry(tab1)
+familyEntryTabOne = tk.Entry(tab1)
+jobEntryTabOne = tk.Entry(tab1)
+
+imgLabelTabOne = tk.Label(tab1)
+
+buttonForward = tk.Button(tab1, text="Forward")
+buttonBack = tk.Button(tab1, text="Back")
+
+# === ADD WIDGETS TO GRID ON TAB ONE
+firstLabelTabOne.grid(row=0, column=0, padx=15, pady=15)
+firstEntryTabOne.grid(row=0, column=1, padx=15, pady=15)
+
+familyLabelTabOne.grid(row=1, column=0, padx=15, pady=15)
+familyEntryTabOne.grid(row=1, column=1, padx=15, pady=15)
+
+jobLabelTabOne.grid(row=2, column=0, padx=15, pady=15)
+jobEntryTabOne.grid(row=2, column=1, padx=15, pady=15)
+
+imgLabelTabOne.grid(row=0, column=2, rowspan=3, padx=15, pady=15)
+
+# === WIDGETS FOR TAB TWO
+firstLabelTabTwo = tk.Label(tab2, text="First Name:")
+familyLabelTabTwo = tk.Label(tab2, text="Family Name:")
+jobLabelTabTwo = tk.Label(tab2, text="Job Title:")
+
+firstEntryTabTwo = tk.Entry(tab2)
+familyEntryTabTwo = tk.Entry(tab2)
+jobEntryTabTwo = tk.Entry(tab2)
+
+imgLabelTabTwo = tk.Label(tab2)
+
+buttonCommit = tk.Button(tab2, text="Add Record to Database")
+buttonAddImage = tk.Button(tab2, text="Add Image")
+
+# === ADD WIDGETS TO GRID ON TAB TWO
+firstLabelTabTwo.grid(row=0, column=0, padx=15, pady=15)
+firstEntryTabTwo.grid(row=0, column=1, padx=15, pady=15)
+imgLabelTabTwo.grid(row=0, column=2, rowspan=3, padx=15, pady=15)
+
+familyLabelTabTwo.grid(row=1, column=0, padx=15, pady=15)
+familyEntryTabTwo.grid(row=1, column=1, padx=15, pady=15)
+
+jobLabelTabTwo.grid(row=2, column=0, padx=15, pady=15)
+jobEntryTabTwo.grid(row=2, column=1, padx=15, pady=15)
+
+buttonCommit.grid(row=4, column=1, padx=15, pady=15)
+buttonAddImage.grid(row=4, column=2, padx=15, pady=15)
+
+# Events
+
+tab_parent.bind("<<NotebookTabChanged>>", on_tab_selected)
+
+# Show
+tab_parent.pack(expand=1, fill = "both")
+form.mainloop()
--- ui.py
+++ ui.py
@@ -2,8 +2,11 @@
 # Show information and read input from user
 # Draw output to user
 
-from model_store import model_store
+#from model_store import model_store
+import tkinter as tk
+from tkinter import ttk
 
+'''
 def choose_model(model):
 	while True:
 		tag = input('Choose a model: (SVM or LR) ')
@@ -14,7 +17,7 @@ def choose_model(model):
 def configure_model(model):
 	args = model.get_arguments()
 	values = {}
-	for arg in args
+	for arg in args:
 		val = input('%s (%s): ' %(arg[0], arg[1]))
 		values[arg[0]] = val
 	model.set_arguments(values)
@@ -30,11 +33,31 @@ def feed_data(model):
 
 def control_model(model):
 	print('This work is not done')
+'''
 
 if __name__ == "__main__":
+	# Main form
+	form = tk.Tk()
+	form.title("Model visualizer")
+	form.geometry("500x280")
 	
-	model = model_store()
-	choose_model(model)
-	configure_model(model)
-	feed_data(model)
-	control_model(model)
+	# Create tabs
+	tab_parent = ttk.Notebook(form)
+	tab_model = ttk.Frame(tab_parent)
+	tab_config = ttk.Frame(tab_parent)
+	tab_data = ttk.Frame(tab_parent)
+	tab_visual = ttk.Frame(tab_parent)
+
+	# Assign tabs to parents
+	tab_parent.add(tab_model, text = "Model")
+	tab_parent.add(tab_config, text = "Configure")
+	tab_parent.add(tab_data, text = "Data")
+	tab_parent.add(tab_visual, text = "Visualization")
+
+	# Widgets for tab_model
+	title_tab_model = tk.Label(tab_model, text = "Choose a model")
+	
+
+	# Show
+	tab_parent.pack(expand=1, fill="both")
+	form.mainloop()
